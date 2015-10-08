24c8e98cb88230c612ee6dcccdf184222b2f7e98
Bistra Shindrova <bistra_mwcf@yahoo.com>
2015-10-08 15:10:19 +0300

    Създашане на програма Hello Aula

---
+++ helloAula/helloAula/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace helloAula
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello Aula");
+			Console.ReadKey ();
+		}
+	}
+}
