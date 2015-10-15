1b1c2e1e9e777e025c79ce202b92601c79672517
Bistra Shindrova <bistra_mwcf@yahoo.com>
2015-10-15 10:50:20 +0300

    Работа с int и string

---
+++ modul2/ReadFromCommandLine/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ReadFromCommandLine
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//Дефиниране на променлиши
+			int a = 5, b = 0;
+
+			//Въвеждане на входни параметри
+			Console.Write ( "Моля въведете а: " );
+			b = Convert.ToInt32 (Console.ReadLine ());
+
+			//Печат на резултат
+			Console.WriteLine ( "Резултатът а + b е:" + ( a + b ).ToString() + "\n\n\n");
+
+			string test = "1";
+			test += ", 2";
+			test += ", 100";
+			test += ", 4";
+			test += ", 5";
+
+			//Задача 1 към Урок 6
+			string test1 = "Hello";
+			string test2 = " World";
+			object prom = test1 + test2;
+			string prom1 = Convert.ToString ( prom );
+
+			Console.WriteLine (prom + ("\n"));
+			Console.WriteLine (prom1 + ("\n"));
+
+			//Задача 2 към урок 6
+			int c = 0; 
+			string word = null;
+			string word1 = Convert.ToString (c);
+			Console.WriteLine ("Моля въведете 1 число: ");
+			c = Convert.ToInt32 (Console.ReadLine ());
+			Console.WriteLine ("Моля въведете 1 дума: ");
+			word = (Console.ReadLine ());
+
+			Console.WriteLine ("Броят на символите е: " + ( word + word1).Length + ("\n\n"));
+			//Как да изведа броя на символите?????
+
+
+			Console.Write ( "Събиране с +=: " + test + ("\n\n"));
+
+			test = test.Replace (", ", ";");
+			Console.Write ( "Работа с Replace: " + test + ("\n\n"));
+
+			Console.Write ( "3-ти знак: " + test.Split (';')[2] + ("\n\n"));
+
+
+		}
+	}
+}
---
+++ modul2/modul2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace modul2
+{
+	class MainClass
+	{
+		public static void Main ( )
+		{
+			//Дефиниране на променливи
+			int a = 0, b = 0;
+			int sum = 0;
+			string program_name = "Module02 - Examples";
+			string program_version = "1.0";
+
+			//Събиране на числа
+			a = 5; b = 10; sum = a + b;
+
+			//Данни за програмата
+			Console.WriteLine ( program_name );
+			Console.WriteLine ("Версия: " + program_version + "\n\n");
+
+			//Писане в конзола
+			Console.Write(a);
+			Console.Write (" + ");
+			Console.Write (b);
+			Console.Write ('=');
+			Console.WriteLine ( sum );
+
+			//Допълнителни оператори
+			Console.WriteLine ("\n Използване на += ");
+			a += b;
+			Console.WriteLine ( a );
+
+			Console.WriteLine ("\n Използване на -= ");
+			a -= b;
+			Console.WriteLine ( a );
+
+			Console.WriteLine ("\n Използване на *= ");
+			a *= b;
+			Console.WriteLine ( a );
+
+			Console.WriteLine ("\n Използване на ++ ");
+			a ++ ;
+			Console.WriteLine ( a );
+
+			Console.WriteLine ("Стандартно делене " + "14/4" ); 
+			Console.WriteLine ( 14/4 ); //резулт. 3, от целоч. делене на 14 и 4
+			Console.WriteLine ("Остатък от делене "); 
+			Console.WriteLine ((14 % 4).ToString ()); //% връща ост. от целоч. делене
+		}
+	}
+}
